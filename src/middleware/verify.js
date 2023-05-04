b59e87221cd6ec1ad61aedaafc153e5977e9ae9a
Vinh7252 <ngducvinh72@gmail.com>
2023-05-04 16:34:49 +0700

    update verify

--- src/middleware/verify.js
+++ src/middleware/verify.js
@@ -1,12 +1,16 @@
+const jwt = require("jsonwebtoken");
 
-function verify(req, res, next){
-    const token = (req.headers.authorization).spilt(" ")[1]
-    const isAuth = false
-    if (!isAuth){
-        next(new Error("Access denied"))
-    }
-    next()
+function verify(req, res, next) {
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
 
+  if (!token) {
+    next(new Error("Access denied"));
+  }
+
+  const decoded = jwt.verify(token, "bimat");
+  req.userId = decoded.user_id;
+  next();
 }
 
 module.exports = verify;
